1e00e25c2a40234cdbe0f2fdc4d321db857bdda4
Franc <biribo.francesco@studenti-ittfedifermi.edu.it>
2022-04-28 23:35:03 +0200

    Login activity added

--- app/src/main/java/com/example/multe/MenuActivity.java
+++ app/src/main/java/com/example/multe/MenuActivity.java
@@ -2,17 +2,46 @@ package com.example.multe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
 import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+    private long mLastClickTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         getWindow().setNavigationBarColor(Color.BLACK);
+
+        View.OnClickListener info = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (SystemClock.elapsedRealtime() - mLastClickTime < 1000) return;
+                mLastClickTime = SystemClock.elapsedRealtime();
+
+                startActivity(new Intent(MenuActivity.this, InfoActivity.class));
+            }
+        };
+        View.OnClickListener login = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (SystemClock.elapsedRealtime() - mLastClickTime < 1000) return;
+                mLastClickTime = SystemClock.elapsedRealtime();
+
+                startActivity(new Intent(MenuActivity.this, LoginActivity.class));
+            }
+        };
+
+        (findViewById(R.id.binfo1)).setOnClickListener(info);
+        (findViewById(R.id.binfo2)).setOnClickListener(info);
+
+        (findViewById(R.id.bout1)).setOnClickListener(login);
+        (findViewById(R.id.bout2)).setOnClickListener(login);
     }
 }
